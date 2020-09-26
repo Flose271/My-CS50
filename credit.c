5b9f546a25a1a01d508cee5ea24920aa3529280e
Flose271 <Flose271@users.noreply.github.com>
2020-09-26 14:01:12 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -22,23 +22,23 @@ int main(void)
     //This will keep track of the sum of credit card numbers.
     int sum = 0;
 
-    for(int i = 1; i<len+1; i++)
+    for (int i = 1; i < len + 1; i++)
     {
         //Get the ith digit from the back 
         int digit = digitof(n, i);
         //If i is even, we double the digit and add it to the sum.
-        if(i%2 == 0)
+        if (i % 2 == 0)
         {
             //If 2*digit is under 10, we can just add it to the sum.
-            if(2*digit<10)
+            if (2 * digit < 10)
             {
-                sum += 2*digit;
+                sum += 2 * digit;
             }
             //If it's over 10, we need to add the digits individually as so.
             else
             {
                 sum += 1;
-                sum += (2*digit)%10;
+                sum += (2 * digit) % 10;
             }
             
         }
@@ -53,22 +53,22 @@ int main(void)
     //printf("%i",sum);
     
     //If the sum doesn't end in a zero then it is invalid
-    if(sum%10 != 0)
+    if (sum % 10 != 0)
     {
         printf("INVALID\n");
     }
     //If the number is 15 digits long, first digit 3 and second digit 4 or 7
-    else if(length(n) == 15 && digitof(n,15) == 3 && (digitof(n,14) == 4 || digitof(n,14) == 7))
+    else if (length(n) == 15 && digitof(n, 15) == 3 && (digitof(n, 14) == 4 || digitof(n, 14) == 7))
     {
         printf("AMEX\n");
     }
     //If the number is 16 digits long, first digit 5 and second digit between 1 and 5
-    else if(length(n) == 16 && digitof(n,16) == 5 && 0<digitof(n,15) && digitof(n,15)<6)
+    else if (length(n) == 16 && digitof(n, 16) == 5 && 0 < digitof(n, 15) && digitof(n, 15) < 6)
     {
         printf("MASTERCARD\n");
     }
     //If the number is 13 digits or 16 digits long and starts with a 4
-    else if((length(n) == 13 && digitof(n,13) == 4) || (length(n) == 16 && digitof(n,16) == 4))
+    else if ((length(n) == 13 && digitof(n, 13) == 4) || (length(n) == 16 && digitof(n, 16) == 4))
     {
         printf("VISA\n");
     }
@@ -88,22 +88,22 @@ int length(long n)
     long bp10;
     do
     {
-        sp10 = pow(10,i);
+        sp10 = pow(10, i);
         i++;
-        bp10 = pow(10,i);
+        bp10 = pow(10, i);
     }
     //This repeats until both n/sp10 != 0 and n/bp10 == 0, which then we know it is of length i.
-    while(n/sp10 == 0 || n/bp10 != 0);
+    while (n / sp10 == 0 || n / bp10 != 0);
     return i;
 }
 
 int digitof(long n, int d)
 {
     //Let cutnumber be the last d digits of n.
-    long power = pow(10,d);
-    long cutnumber = n%power;
+    long power = pow(10, d);
+    long cutnumber = n % power;
     //Then the first digit of cutnumber is found.
     //This gives us our 'dth' digit.
-    int digit = cutnumber/pow(10,d-1);
+    int digit = cutnumber / pow(10, d - 1);
     return digit;
 }
