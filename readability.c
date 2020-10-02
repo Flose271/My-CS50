82be3f77f3701e41d282efb7e80ee796f0ee082b
Flose271 <Flose271@users.noreply.github.com>
2020-10-02 13:48:34 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -37,15 +37,15 @@ int main(void)
     
     if(index > 16)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
     else if(index < 1)
     {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
     else
     {
-        printf("Grade %.0f", index);
+        printf("Grade %.0f\n", index);
     }
 
 }
