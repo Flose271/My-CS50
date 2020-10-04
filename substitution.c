5c091a9598093589597f9aa90c8c74d24fa74009
Flose271 <Flose271@users.noreply.github.com>
2020-10-04 12:48:37 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -8,19 +8,23 @@ int timesin(int item, string s);
 
 int main(int argc, string arcv[])
 {
+    //Tests if only one word (the key) was given
     if (argc != 2)
     {
         printf("Only a single command line argument expected\n");
         return 1;
     }
     
+    //Sets the key to the argment given
     string key = arcv[1];
     
+    //Tests if the key is 26 letters long
     if (strlen(arcv[1]) != 26)
     {
         printf("Key must be a string of length 26\n");
         return 1;
     }
+    //Tests if the key only contains valid characters
     if (validstring(key) == false)
     {
         printf("All characters in key must be valid letters\n");
@@ -29,9 +33,10 @@ int main(int argc, string arcv[])
     
     string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
-    for (int i = 0; i<26; i++)
+    //Tests if all the letters in the alphabet appear in the key exactly once
+    for (int i = 0; i < 26; i++)
     {
-        if(timesin(alphabet[i],key) != 1)
+        if (timesin(alphabet[i], key) != 1)
         {
             printf("All letters must appear exactly once");
             return 1;
@@ -44,11 +49,13 @@ int main(int argc, string arcv[])
     
     for (int i = 0; i < strlen(plaintext); i++)
     {
-        if(('A' <= plaintext[i] && plaintext[i] <= 'Z') || ('a' <= plaintext[i] && plaintext[i] <= 'z'))
+        //If the character in plaintext is a valid letter
+        if (('A' <= plaintext[i] && plaintext[i] <= 'Z') || ('a' <= plaintext[i] && plaintext[i] <= 'z'))
         {
+            //Converts the value in the key to its numerical value in the alphabet
             int plainchar = plaintext[i];
             int keyvalue = 0;
-            if(isupper(plainchar) != 0)
+            if (isupper(plainchar) != 0)
             {
                 keyvalue = plainchar - 65;
             }
@@ -56,10 +63,13 @@ int main(int argc, string arcv[])
             {
                 keyvalue = plainchar - 97;
             }
-        
+            
+            //Finds the difference between the current position of the key and where it should be
             int difference = toupper(key[keyvalue]) - toupper(plainchar);
+            //Prints the new character taking that difference into account
             printf("%c", plainchar + difference);
         }
+        //If not a valid letter of the alphabet then print it as normal
         else
         {
             printf("%c", plaintext[i]);
@@ -71,9 +81,10 @@ int main(int argc, string arcv[])
 
 }
 
+//Checks whether all characters in a string are valid
 bool validstring(string s)
 {
-    for (int i = 0; i<26; i++)
+    for (int i = 0; i < 26; i++)
     {
         if (!(s[i] >= 'A' && s[i] <= 'Z') && !(s[i] >= 'a' && s[i] <= 'z'))
         {
@@ -83,12 +94,13 @@ bool validstring(string s)
     return true;
 }
 
+//Finds the number of times a letter appears in a string
 int timesin(int item, string s)
 {
     int count = 0;
     for (int i = 0; i < strlen(s); i++)
     {
-        if(tolower(item) == s[i] || toupper(item) == s[i])
+        if (tolower(item) == s[i] || toupper(item) == s[i])
         {
             count += 1;
         }
