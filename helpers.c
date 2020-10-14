a28c5fdfcd00d5ac6b171d4282c5c7c37a92b1b8
Flose271 <Flose271@users.noreply.github.com>
2020-10-14 16:11:35 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -42,18 +42,65 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         }
     }
     
-    
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    
+    //Make a copy of the original image
+    RGBTRIPLE copy[height][width];
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            copy[row][col] = image[row][col];
+        }
+    }
+    
+    //Find a pixel
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            //Now working on level of individual pixel
+            //xd is x delta, yd is y delta (from pixel)
+            
+            int redsum = 0;
+            int greensum = 0;
+            int bluesum = 0;
+            int total = 0;
+            
+            for (int xd = -1; xd < 2; xd++)
+            {
+                for (int yd = -1; yd < 2; yd++)
+                {
+                    //If this reffered to pixel is valid, add its sum to total
+                    if(col + xd >= 0 && col + xd < width && row + yd >= 0 && row + yd < height)
+                    {
+                        redsum += copy[row+yd][col+xd].rgbtRed;
+                        greensum += copy[row+yd][col+xd].rgbtGreen;
+                        bluesum += copy[row+yd][col+xd].rgbtBlue;
+                        total += 1;
+                    }
+                }
+            }
+            
+            //Now change corresponding pixel in original image to the average value
+            image[row][col].rgbtRed = round((float) redsum/total);
+            image[row][col].rgbtGreen = round((float) greensum/total);
+            image[row][col].rgbtBlue = round((float) bluesum/total);
+
+        }
+    }
+    
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
