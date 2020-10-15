23ddfe1fd40cc984d1428ddb8848ff3c4fb5eac1
Flose271 <Flose271@users.noreply.github.com>
2020-10-15 11:48:39 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -12,7 +12,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             int red = image[row][col].rgbtRed;
             int green = image[row][col].rgbtGreen;
             int blue = image[row][col].rgbtBlue;
-            int grey = round((red+green+blue)/3.0);
+            int grey = round((red + green + blue) / 3.0);
             image[row][col].rgbtRed = grey;
             image[row][col].rgbtGreen = grey;
             image[row][col].rgbtBlue = grey;
@@ -33,13 +33,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             int green = image[row][col].rgbtGreen;
             int blue = image[row][col].rgbtBlue;
             
-            image[row][col].rgbtRed = image[row][width-1-col].rgbtRed;
-            image[row][col].rgbtGreen = image[row][width-1-col].rgbtGreen;
-            image[row][col].rgbtBlue = image[row][width-1-col].rgbtBlue;
+            image[row][col].rgbtRed = image[row][width - 1 - col].rgbtRed;
+            image[row][col].rgbtGreen = image[row][width - 1 - col].rgbtGreen;
+            image[row][col].rgbtBlue = image[row][width - 1 - col].rgbtBlue;
             
-            image[row][width-1-col].rgbtRed = red;
-            image[row][width-1-col].rgbtGreen = green;
-            image[row][width-1-col].rgbtBlue = blue;
+            image[row][width - 1 - col].rgbtRed = red;
+            image[row][width - 1 - col].rgbtGreen = green;
+            image[row][width - 1 - col].rgbtBlue = blue;
         }
     }
     
@@ -78,20 +78,20 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 for (int yd = -1; yd < 2; yd++)
                 {
                     //If this reffered to pixel is valid, add its sum to total
-                    if(col + xd >= 0 && col + xd < width && row + yd >= 0 && row + yd < height)
+                    if (col + xd >= 0 && col + xd < width && row + yd >= 0 && row + yd < height)
                     {
-                        redsum += copy[row+yd][col+xd].rgbtRed;
-                        greensum += copy[row+yd][col+xd].rgbtGreen;
-                        bluesum += copy[row+yd][col+xd].rgbtBlue;
+                        redsum += copy[row + yd][col + xd].rgbtRed;
+                        greensum += copy[row + yd][col + xd].rgbtGreen;
+                        bluesum += copy[row + yd][col + xd].rgbtBlue;
                         total += 1;
                     }
                 }
             }
             
             //Now change corresponding pixel in original image to the average value
-            image[row][col].rgbtRed = round((float) redsum/total);
-            image[row][col].rgbtGreen = round((float) greensum/total);
-            image[row][col].rgbtBlue = round((float) bluesum/total);
+            image[row][col].rgbtRed = round((float) redsum / total);
+            image[row][col].rgbtGreen = round((float) greensum / total);
+            image[row][col].rgbtBlue = round((float) bluesum / total);
 
         }
     }
@@ -132,30 +132,30 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 for (int yd = -1; yd < 2; yd++)
                 {
                     //If this reffered to pixel is valid
-                    if(col + xd >= 0 && col + xd < width && row + yd >= 0 && row + yd < height)
+                    if (col + xd >= 0 && col + xd < width && row + yd >= 0 && row + yd < height)
                     {
-                        xred += (xd)*(2-abs(yd))*copy[row+yd][col+xd].rgbtRed;
-                        xgreen += (xd)*(2-abs(yd))*copy[row+yd][col+xd].rgbtGreen;
-                        xblue += (xd)*(2-abs(yd))*copy[row+yd][col+xd].rgbtBlue;
+                        xred += (xd) * (2 - abs(yd)) * copy[row + yd][col + xd].rgbtRed;
+                        xgreen += (xd) * (2 - abs(yd)) * copy[row + yd][col + xd].rgbtGreen;
+                        xblue += (xd) * (2 - abs(yd)) * copy[row + yd][col + xd].rgbtBlue;
                         
-                        yred += (yd)*(2-abs(xd))*copy[row+yd][col+xd].rgbtRed;
-                        ygreen += (yd)*(2-abs(xd))*copy[row+yd][col+xd].rgbtGreen;
-                        yblue += (yd)*(2-abs(xd))*copy[row+yd][col+xd].rgbtBlue;
+                        yred += (yd) * (2 - abs(xd)) * copy[row + yd][col + xd].rgbtRed;
+                        ygreen += (yd) * (2 - abs(xd)) * copy[row + yd][col + xd].rgbtGreen;
+                        yblue += (yd) * (2 - abs(xd)) * copy[row + yd][col + xd].rgbtBlue;
                         
-                        int red = round(sqrt(pow(xred,2) + pow(yred,2)));
-                        if(red > 255)
+                        int red = round(sqrt(pow(xred, 2) + pow(yred, 2)));
+                        if (red > 255)
                         {
                             red = 255;
                         }
                         
-                        int green = round(sqrt(pow(xgreen,2) + pow(ygreen,2)));
-                        if(green > 255)
+                        int green = round(sqrt(pow(xgreen, 2) + pow(ygreen, 2)));
+                        if (green > 255)
                         {
                             green = 255;
                         }
                         
-                        int blue = round(sqrt(pow(xblue,2) + pow(yblue,2)));
-                        if(blue > 255)
+                        int blue = round(sqrt(pow(xblue, 2) + pow(yblue, 2)));
+                        if (blue > 255)
                         {
                             blue = 255;
                         }
